52b707b43cbb911aee8bd8445f9c2e33fb225bf8
01jungseungmin <89935651+01jungseungmin@users.noreply.github.com>
2026-08-12 15:38:23 +0900

    feature: 회원 상태에 @Version 낙관적 락 적용

--- backend/src/main/java/com/eeum/eeum/domain/account/entity/Account.java
+++ backend/src/main/java/com/eeum/eeum/domain/account/entity/Account.java
@@ -81,6 +81,10 @@ public class Account extends BaseEntity {
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
+    @Version
+    @Column(name = "version", nullable = false, columnDefinition = "BIGINT NOT NULL DEFAULT 0")
+    private Long version;
+
     public static Account createUser(
             String email,
             String encodedPassword,
